7e6a8eb9322ca8bcb51087ba2e6e91f4e6a53a9b
Marcin Michalak <marcin.michalak@decerto.com>
2021-03-11 21:06:12 +0100

    added template instead template url

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
