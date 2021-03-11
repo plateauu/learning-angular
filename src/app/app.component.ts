6a64ffe819a2947216b26cef055a5a1b57c57efc
Marcin Michalak <marcin.michalak@decerto.com>
2021-03-11 22:12:33 +0100

    after styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles: [
+    `h3 {
+      color: dodgerblue;
+      font-weight: bolder;
+    }`
+  ]
 })
 export class AppComponent {
 }
