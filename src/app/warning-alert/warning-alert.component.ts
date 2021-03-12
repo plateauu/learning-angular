2bca5415ec7011752c7ef68a64a3121902031326
plateauu <plateauu@o2.pl>
2021-03-12 22:22:03 +0100

    got warning alert

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  message = "Oops... something went wrong"
+  showAlert = true
+
+}
